76a3bc5dbfb7cb0b30a1c10da59df1469f5cfa48
Pedro Jesus <pedrofcjesus@gmail.com>
2026-07-15 20:45:18 +0100

    updated c files

--- 01_C_Exercises/UC607/Ex_49_Scorelog.c
+++ 01_C_Exercises/UC607/Ex_49_Scorelog.c
@@ -38,21 +38,3 @@ int main()
 
     return 0;
 }
-
-// Objetivo : Criar um gerador de log de pontuações de jogos.
-
-// O que o programa deve fazer :
-
-// Pede ao utilizador o nome do jogo(usa fgets e limpa o \n com a técnica do strcspn).
-
-// Pede a pontuação máxima atingida(um número inteiro, validado com o if do scanf).
-
-// Abre um ficheiro chamado historico.txt em modo Append.
-
-// Valida se o ficheiro abriu corretamente.
-
-// Escreve a informação numa linha compacta usando fprintf. Exemplo : Jogo : Tetris | Score : 1500
-
-// (lembra - te de meter o \n no final do fprintf para que o próximo registo mude de linha).
-
-// Fecha o ficheiro e avisa o utilizador.
--- 01_C_Exercises/UC607/Ex_51_Scorelog_read_copy.c
+++ 01_C_Exercises/UC607/Ex_51_Scorelog_read_copy.c
@@ -38,27 +38,3 @@ int main()
 
     return 0;
 }
-
-// Objetivo: Criar um programa que lê o teu ficheiro scorelog.txt e
-
-// cria um novo ficheiro chamado copia.txt, mas apenas com as linhas que contêm texto, ignorando linhas em branco se existirem.
-
-// O que o programa deve fazer:
-
-// Abre o scorelog.txt em modo de leitura ("r").
-
-// Abre um ficheiro novo chamado copia.txt em modo de escrita ("w").
-
-// Faz a validação de segurança para ambos os ponteiros (FILE *fp_leitura e FILE *fp_escrita).
-
-// Se algum for NULL, fecha o que estiver aberto e aborta.
-
-// Usa o ciclo while com fgets para ler do primeiro ficheiro.
-
-// Dentro do ciclo, antes de escrever, faz uma pequena validação:
-
-// se a linha lida não estiver vazia (ou seja, se o primeiro caractere da linha não for um \n ou um \0),
-
-// escreve essa linha no ficheiro de cópia usando fprintf.
-
-// Fecha os dois ficheiros no final.
